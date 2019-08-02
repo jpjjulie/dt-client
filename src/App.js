67717a508c9e046e6b1071713630817762bff343
avesk <averykushner@gmail.com>
2019-08-01 18:36:20 -0700

    mucking around with react-strap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import React from "react";
-import Dashboard from "aero-ui/Dashboard";
+import React, { useState } from "react";
 
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+const Dashboard = () => {
+  const [isOpen, setToggleState] = useState(false);
+  const toggle = () => {
+    setToggleState(!isOpen ? true : false);
+  };
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">
+          reactstrap
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} className="mr-2" />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 const App = () => <Dashboard />;
 
 export default App;
